7d06fe69b37e438196636d5461acf86e5c185a6c
diliprahul <rahuldilip231@gmail.com>
2026-07-11 18:56:43 +0530

    Files added Successfully

---
+++ Context_api/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import UserContextProvider from './Context/UserContextProvider'
+import Profile from './components/Profile'
+import Login from './components/Login'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+    <h1>React Project</h1>
+    <Login/>
+    <Profile/> 
+    </UserContextProvider>
+  )
+}
+
+export default App
